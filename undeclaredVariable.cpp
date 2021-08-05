0afc75e30ee528ff403d427b7b7c1716ef99591b
connolrc <connolrc@mail.uc.edu>
2021-08-04 21:43:45 -0400

    CS3003Summer2021FinalProject

---
+++ undeclaredVariable.cpp
@@ -0,0 +1,15 @@
+// undeclaredIdentifier.cpp
+int x, y, z; 
+int main () {
+	sex Alfred, Mindy; 
+	Alfred = male; 
+	Mindy = female; 
+	
+	ifnot (Alfred == Mindy) {
+		x = 1; 
+	}	
+			
+	x = 2; 
+	y = 3; 
+	z = x +* y; 
+}
